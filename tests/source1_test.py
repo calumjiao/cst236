b22c9d915d4427c76caf0cd52ee4b034f1e262dc
Calum Jiao <calumjiao@gmail.com>
2015-04-10 14:55:06 -0400

    ready for Drone.IO

--- tests/source1_test.py
+++ tests/source1_test.py
@@ -12,4 +12,26 @@ class TestGetTriangleType(TestCase):
 
     def test_get_triangle_scalene_all_int(self):
         result = get_triangle_type(1, 2, 3)
-        self.assertEqual(result, 'equilateral')
+        self.assertEqual(result, 'scalene')
+        
+    def test_get_triangle_isosceles_all_int(self):
+        result = get_triangle_type(2, 2, 3)
+        self.assertEqual(result, 'isosceles')
+        
+    def test_get_triangle_invalid_some_letter(self):
+        result = get_triangle_type(1, 2, '3')
+        self.assertEqual(result, 'invalid')
+        
+    def test_get_triangle_invalid_some_negative(self):
+        result = get_triangle_type(1, 2, -3)
+        self.assertEqual(result, 'invalid')   
+        
+    def test_get_triangle_scalene_list(self):
+        result = get_triangle_type([1,2,3])
+        self.assertEqual(result, 'scalene') 
+        
+    def test_get_triangle_equilateral_dict(self):
+        result = get_triangle_type({'a':3,'b':3,'c':3})
+        self.assertEqual(result, 'equilateral') 
+        
+ 
